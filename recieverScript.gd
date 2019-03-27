59e5d1e50d2dda6ac5db663f456e79c24a6c3437
nikk charalampidis <nikk.ch@protonmail.com>
2019-03-27 18:17:46 +0200

    can now export to macos

--- recieverScript.gd
+++ recieverScript.gd
@@ -12,7 +12,7 @@ onready var dancer = preload("res://dancer.tscn")
 
 func _ready():
 	party = get_node("../Particles")
-	oscrcv = load("res://addons/gdosc/bin/gdoscreceiver.gdns").new()
+	oscrcv = load("res://bin/gdoscreceiver.gdns").new()
 	oscrcv.max_queue( 20 )
 	oscrcv.avoid_duplicate( true )	# receiver will only keeps the "latest" message for each address
 	oscrcv.setup( 14000 )			# listening to port 14000
